5821d867e1179077f94202a581be5f39c615f3e9
Christoph Sax <christoph.sax@mailbox.org>
2022-10-15 17:48:07 +0200

    Add license headers

--- grammar.js
+++ grammar.js
@@ -1,3 +1,25 @@
+/*
+   Copyright (c) 2022 Christoph Sax
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+ */
+
 const PREC = {
   command: -1,
   path: -1,
@@ -11,6 +33,7 @@ const PREC = {
   add_sub: 17,
   mul_div_mod: 18,
   shift: 19,
+  unary: 20,
   address: 100,
   time: 100
 }
@@ -301,6 +324,11 @@ module.exports = grammar({
       repeat($._blank),
       $._expression
     ),
+
+   unary_expression: $ => prec.left(PREC.unary, seq(
+      field('operator', choice('!', '~', '-', '+')),
+      field('argument', $._expression)
+    )),
 
     binary_expression: $ => {
       const operators = [
