8dc1179b5eb361dccd61ecae92f4804a500e5550
Christoph Sax <c_sax@mailbox.org>
2025-02-08 20:09:28 +0100

    Add Zig language bindings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+
+- Switch to Tree-sitter v0.25.1
+- Add Zig language bindings
+
 ## [5.0.3] - 2025-01-17
 
 ### Added
--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -22,8 +22,14 @@ EOF
 readonly version_python="$version_rust"
 readonly version_treesitter="$version_node"
 
+readonly version_zig=$(cat << EOF
+.version = "$1"
+EOF
+)
+
 sed --in-place "s#\(.*\)\<VERSION\s\+\".*\$#\1$version_cmake#" "CMakeLists.txt"
 sed --in-place "s#\(.*\)\"version\":.*\$#\1$version_node#" "package.json"
 sed --in-place "s#\(.*\)version\s*=.*\$#\1$version_python#" "pyproject.toml"
 sed --in-place "s#\(.*\)version\s*=.*\$#\1$version_rust#" "Cargo.toml"
 sed --in-place "s#\(.*\)\"version\":.*\$#\1$version_treesitter#" "tree-sitter.json"
+sed --in-place "s#\(.*\)\.version\s*=.*\$#\1$version_zig#" "build.zig.zon"
