3ce3580e57f20714187aa4a5e6d07a2078a6aa7d
Christoph Sax <c_sax@mailbox.org>
2025-07-16 20:45:50 +0200

    Fix Python bindings

--- bindings/python/tree_sitter_t32/binding.c
+++ bindings/python/tree_sitter_t32/binding.c
@@ -2,10 +2,10 @@
 
 typedef struct TSLanguage TSLanguage;
 
-TSLanguage *tree_sitter_template(void);
+TSLanguage *tree_sitter_t32(void);
 
 static PyObject* _binding_language(PyObject *Py_UNUSED(self), PyObject *Py_UNUSED(args)) {
-    return PyCapsule_New(tree_sitter_template(), "tree_sitter.Language", NULL);
+    return PyCapsule_New(tree_sitter_t32(), "tree_sitter.Language", NULL);
 }
 
 static PyMethodDef methods[] = {
