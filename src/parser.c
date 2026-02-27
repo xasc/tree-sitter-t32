05d4e5469fd50b08981e6e67ea2c9011b2b76206
Christoph Sax <c_sax@mailbox.org>
2026-02-27 20:21:48 +0100

    Add OIDC token for npm trusted publishing

--- src/parser.c
+++ src/parser.c
@@ -192703,7 +192703,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_t32(void) {
     .metadata = {
       .major_version = 7,
       .minor_version = 2,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
